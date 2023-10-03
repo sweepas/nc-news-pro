7ca583b3abe79cb3c1db068cb3777dd5c00b6fbd
sweepas <47362063+sweepas@users.noreply.github.com>
2023-10-03 13:28:32 +0100

    section, get articles by ID

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const { getAllTopics, getApiInfo } = require("./controllers/topics.contollers");
+const { getArticlesByID } = require("./controllers/articles.conrollers");
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors,
+} = require("./controllers/errors.controllers");
+
 const app = express();
 
 app.get("/api", getApiInfo);
 
 app.get("/api/topics", getAllTopics);
 
+app.get("/api/articles/:article_id", getArticlesByID);
+
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "wrong path" });
 });
 
-app.use((err, req, res, next) => {
-  if (err) res.status(500).send({ msg: "internal server error" });
-});
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
